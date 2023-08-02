a8653ede91ff3fc8158763bf64ce8edf55006992
ptnmay <pittinansaeyang@gmail.com>
2023-08-03 04:01:46 +0700

    g++

---
+++ Practice_CPP/try_cpp.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   try_cpp.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psaeyang <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/03 03:54:13 by psaeyang          #+#    #+#             */
+/*   Updated: 2023/08/03 03:56:57 by psaeyang         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  std:: cout << "The length of the txt string is: " << txt.size();
+  return 0;
+}
