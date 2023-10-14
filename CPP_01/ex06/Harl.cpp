2d32785d4f7f62c66fe740bcc88af799f6a1e797
ptnmay <pittinansaeyang@gmail.com>
2023-10-15 01:21:58 +0700

    ex06

--- CPP_01/ex06/Harl.cpp
+++ CPP_01/ex06/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: psaeyang <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/14 02:01:29 by psaeyang          #+#    #+#             */
-/*   Updated: 2023/10/14 02:06:42 by psaeyang         ###   ########.fr       */
+/*   Updated: 2023/10/15 01:21:03 by psaeyang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,37 +15,35 @@
 
 void	Harl::debug(void)
 {
-	std::cout << BWHT"DEBUG :: " << RESET;
+	std::cout << BGRN"DEBUG" << BWHT" :: " << RESET;
 	std::cout << BGRN"I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << RESET << std::endl;
 }
 
 void	Harl::info(void)
 {
-	std::cout << BWHT"INFO :: " << RESET;
+	std::cout << BCYN"INFO" << BWHT" :: " << RESET;
 	std::cout << BCYN"I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << RESET << std::endl;
 }
 
 void	Harl::warning(void)
 {
-	std::cout << BWHT"WARNING :: " << RESET;
+	std::cout << BMAG"WARNING" << BWHT" :: " << RESET;
 	std::cout << BMAG"I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << RESET << std::endl;
 }
 
 void	Harl::error(void)
 {
-	std::cout << BWHT"ERROR :: " << RESET;
+	std::cout << BYEL"ERROR" << BWHT" :: " << RESET;
 	std::cout << BYEL"This is unacceptable! I want to speak to the manager now." << RESET << std::endl;
 }
 
 void	Harl::complain(std::string level)
 {
-	switch (expression)
+	std::string levels[] = {"debug", "info", "warning", "error"};
+	void (Harl::*ft[])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	for (int i = 0; i < 4; i++)
 	{
-	case /* constant-expression */:
-		/* code */
-		break;
-	
-	default:
-		break;
+		if (level == levels[i])
+			(this->*ft[i])();
 	}
 }
--- CPP_01/ex06/main.cpp
+++ CPP_01/ex06/main.cpp
@@ -6,7 +6,7 @@
 /*   By: psaeyang <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/14 01:59:47 by psaeyang          #+#    #+#             */
-/*   Updated: 2023/10/14 02:01:12 by psaeyang         ###   ########.fr       */
+/*   Updated: 2023/10/15 01:17:39 by psaeyang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,28 @@ int	main(int ac, char *av[])
 		std::cerr << BRED"Usage: " << av[0] << " <log_level>" << std::endl;
 		return 1;
 	}
+	Harl harl;
+	std::string lev[] = {"debug", "info", "warning", "error"};
 	
+	int n = -1;
+	for (int i = 0; i < 4; i++)
+	{
+		if (lev[i] == av[1])
+			n = i;
+	}
+	switch (n)
+	{
+		case 0:
+			harl.complain("debug");
+		case 1:
+			harl.complain("info");
+		case 2:
+			harl.complain("warning");
+		case 3:
+			harl.complain("error");
+			break;
+		default:
+			std::cout << BRED"[ Probably complaining about insignificant problems ]" << RESET << std::endl;
+	}
+	return 0;
 }
