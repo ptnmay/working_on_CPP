5590f3a72848b425645fba39731ced2639ed1941
ptnmay <pittinansaeyang@gmail.com>
2023-10-15 01:24:04 +0700

    ex05 fix

--- CPP_01/ex05/Harl.cpp
+++ CPP_01/ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: psaeyang <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 15:58:56 by psaeyang          #+#    #+#             */
-/*   Updated: 2023/10/14 01:33:37 by psaeyang         ###   ########.fr       */
+/*   Updated: 2023/10/15 01:22:14 by psaeyang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,25 @@
 
 void	Harl::debug(void)
 {
-	std::cout << BWHT"DEBUG :: " << RESET;
+	std::cout << BGRN"DEBUG" << BWHT" :: " << RESET;
 	std::cout << BGRN"I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << RESET << std::endl;
 }
 
 void	Harl::info(void)
 {
-	std::cout << BWHT"INFO :: " << RESET;
+	std::cout << BCYN"INFO" << BWHT" :: " << RESET;
 	std::cout << BCYN"I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << RESET << std::endl;
 }
 
 void	Harl::warning(void)
 {
-	std::cout << BWHT"WARNING :: " << RESET;
+	std::cout << BMAG"WARNING" << BWHT" :: " << RESET;
 	std::cout << BMAG"I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << RESET << std::endl;
 }
 
 void	Harl::error(void)
 {
-	std::cout << BWHT"ERROR :: " << RESET;
+	std::cout << BYEL"ERROR" << BWHT" :: " << RESET;
 	std::cout << BYEL"This is unacceptable! I want to speak to the manager now." << RESET << std::endl;
 }
 
