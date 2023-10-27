b1c1cf0838d17cab1339ea4a7ea0d4c33b36afe4
ptnmay <pittinansaeyang@gmail.com>
2023-10-28 02:44:32 +0700

    start ex01

---
+++ CPP_03/ex01/ClapTrap.hpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psaeyang <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/20 00:03:11 by psaeyang          #+#    #+#             */
+/*   Updated: 2023/10/26 01:47:18 by psaeyang         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+#include <iostream>
+#include <string>
+
+# define BBLK "\e[1;30m"
+# define BRED "\e[1;31m"
+# define BGRN "\e[1;32m"
+# define BYEL "\e[1;33m"
+# define BBLU "\e[1;34m"
+# define BMAG "\e[1;35m"
+# define BCYN "\e[1;36m"
+# define BWHT "\e[1;37m"
+# define RESET "\e[0m"
+
+class ClapTrap
+{
+		private:
+				std::string _name;
+				int _hitpoint;
+				int _energy;
+				int _attack;
+		public:
+				ClapTrap();
+				~ClapTrap();
+				ClapTrap(std::string const name);
+				ClapTrap(ClapTrap const &CTcpy);
+				ClapTrap &	operator = (ClapTrap const &CTcpy);
+
+				void attack(const std::string& target);
+				void takeDamage(unsigned int amount);
+				void beRepaired(unsigned int amount);
+};
+
+#endif
---
+++ CPP_03/ex01/ScavTrap.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psaeyang <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/28 02:00:48 by psaeyang          #+#    #+#             */
+/*   Updated: 2023/10/28 02:43:59 by psaeyang         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+#include "ClapTrap.hpp"
+
+# define BBLK "\e[1;30m"
+# define BRED "\e[1;31m"
+# define BGRN "\e[1;32m"
+# define BYEL "\e[1;33m"
+# define BBLU "\e[1;34m"
+# define BMAG "\e[1;35m"
+# define BCYN "\e[1;36m"
+# define BWHT "\e[1;37m"
+# define RESET "\e[0m"
+
+class ScavTrap : public ClapTrap
+{
+		public:
+				ScavTrap();
+				~ScavTrap();
+				ScavTrap(ScavTrap const &STcpy);
+				ScavTrap(std::string const name);
+				ScavTrap & operator = (ScavTrap const &STcpy);
+				void guardGate();
+};
+
+#endif
