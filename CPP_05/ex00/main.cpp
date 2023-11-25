e21cd9da2d462264eb9e415c3d96513982b97de9
ptnmay <pittinansaeyang@gmail.com>
2023-11-26 02:38:49 +0700

    ex00

--- CPP_05/ex00/main.cpp
+++ CPP_05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: psaeyang <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 20:05:46 by psaeyang          #+#    #+#             */
-/*   Updated: 2023/11/19 01:39:14 by psaeyang         ###   ########.fr       */
+/*   Updated: 2023/11/25 00:08:50 by psaeyang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ int main()
 	try
 	{
 		Bureaucrat	test("test", 20);
-		std::cout << BWHT << test.getName() << RESET << std::endl;
-		std::cout << BWHT << test.getGrade() << RESET << std::endl;
+		std::cout << BMAG << "getName:: " << test.getName() << RESET << std::endl;
+		std::cout << BWHT << "getGrade:: " << test.getGrade() << RESET << std::endl;
 		std::cout << BCYN << test << RESET;
 		test.increaseGrade(2);
-		std::cout << BWHT << test.getGrade() << RESET << std::endl;
+		std::cout << BWHT << "getGrade:: " << test.getGrade() << RESET << std::endl;
 		test.decreaseGrade(3);
-		std::cout << BWHT  << test.getGrade() << RESET << std::endl;
+		std::cout << BWHT << "getGrade:: " << test.getGrade() << RESET << std::endl;
 		
 	}
 	catch (std::exception & e)
