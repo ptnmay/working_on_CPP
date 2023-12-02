fbfd170ba4ae525be0981b250ac7bff5d60af946
ptnmay <pittinansaeyang@gmail.com>
2023-12-03 03:01:24 +0700

    working

--- CPP_05/ex01/Form.hpp
+++ CPP_05/ex01/Form.hpp
@@ -6,7 +6,7 @@
 /*   By: psaeyang <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 02:07:02 by psaeyang          #+#    #+#             */
-/*   Updated: 2023/12/01 03:02:21 by psaeyang         ###   ########.fr       */
+/*   Updated: 2023/12/03 02:57:08 by psaeyang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,26 @@ class Form
 
 	public:
 		~Form();
+		Form(Form const& other);
+		Form& operator=(Form const& cpy);
 		Form(std::string const name, int const signGrade, int const execGrade);
+
+		std::string	getName() const;
+		bool		getSign() const;
+		int			getSignG() const;
+		int			getSignExec() const;
+		
+		void		beSigned(Bureaucrat const& bureacrat);
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				const char* what() const throw();
+		};
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				const char* what() const throw();
+		};
 };
 
 #endif
