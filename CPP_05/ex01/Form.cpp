cfa83533016b8fc87d56feac7a4ecf23502ed78d
ptnmay <pittinansaeyang@gmail.com>
2023-12-20 23:52:28 +0700

    -

--- CPP_05/ex01/Form.cpp
+++ CPP_05/ex01/Form.cpp
@@ -6,7 +6,7 @@
 /*   By: psaeyang <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 02:07:06 by psaeyang          #+#    #+#             */
-/*   Updated: 2023/12/03 02:58:04 by psaeyang         ###   ########.fr       */
+/*   Updated: 2023/12/20 23:46:25 by psaeyang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,15 @@ Form::~Form()
 Form::Form(std::string const name, int const signGrade, int const execGrade)
 : _name(name), _sign(false), _signGrade(signGrade), _execGrade(execGrade)
 {
-	
+	if (signGrade < 1 || execGrade < 1)
+		throw Form::GradeTooHighException();
+	else if (signGrade > 150 || execGrade > 150)
+		throw Form::GradeTooLowException();
+	std::cout << BYEL << "[Form] Parametric Constructor Called\n" << RESET;
+}
+
+Form::Form(Form const& other)
+{
+	std::cout << BWHT"| Form | Copy Constructor Called" << RESET << std::endl;
+	*this = other;
 }
