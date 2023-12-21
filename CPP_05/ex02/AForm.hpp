42bf1549bc1b86675079f8c3b8624762841b8ebd
ptnmay <pittinansaeyang@gmail.com>
2023-12-21 15:51:32 +0700

    ex02ing

---
+++ CPP_05/ex02/AForm.hpp
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   AForm.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psaeyang <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/21 15:47:35 by psaeyang          #+#    #+#             */
+/*   Updated: 2023/12/21 15:50:28 by psaeyang         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef AFORM_HPP
+# define AFORM_HPP
+
+# include "Bureaucrat.hpp"
+
+class Bureaucrat;
+
+class AForm
+{
+	private:
+		std::string const _name;
+		bool		_sign;
+		int const	_signGrade;
+		int const	_execGrade;
+
+	public:
+		~AForm();
+		AForm(AForm const& other);
+		AForm& operator=(AForm const& cpy);
+		AForm(std::string const name, int const signGrade, int const execGrade);
+
+		std::string	getName() const;
+		bool		getSign() const;
+		int			getSignG() const;
+		int			getSignExec() const;
+		
+		void		beSigned(Bureaucrat const& bureacrat);
+		
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw()
+				{
+					return ("Grade too high");
+				}
+		};
+		
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw()
+				{
+					return ("Grade too Low");
+				}
+		};
+};
+
+std::ostream&	operator<<(std::ostream& print, AForm const& show);
+
+#endif
---
+++ CPP_05/ex02/main.cpp
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psaeyang <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/21 15:46:48 by psaeyang          #+#    #+#             */
+/*   Updated: 2023/12/21 15:47:58 by psaeyang         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+#include "Form.hpp"
+
+
+struct Param
+{
+	std::string	a;
+	int			b;
+	int			c;
+};
+
+void	testForm()
+{
+	Param	param[] = {
+		{"Warrior", 0, 20},
+		{"Archer", 20, 0},
+		{"Mage", 151, 20},
+		{"Acolyte", 20, 151},
+		{"Merchant", 0, 0},
+		{"Thief", 151, 151}};
+	for (int i = 0; i < 6; i++)
+	{
+		try
+		{
+			Form	form(param[i].a, param[i].b, param[i].c);
+		}
+		catch (std::exception & e)
+		{
+			std::cout << BRED << e.what() << RESET << std::endl;
+		}
+	}
+}
+
+int	main(void)
+{
+	// testForm();
+	std::cout << std::endl;
+	try
+	{
+		Form		form("Warrior", 30, 20);
+		Bureaucrat	obj1("Mark", 30);
+		Bureaucrat	obj2("Mos", 31);
+
+		std::cout << form << std::endl;
+		std::cout << obj1 << std::endl;
+		std::cout << obj2 << std::endl;
+
+		obj1.signForm(form);
+		std::cout << form << std::endl;
+		obj2.signForm(form);
+	}
+	catch (std::exception & e)
+	{
+		std::cout << BRED << e.what() << RESET << std::endl;
+	}
+	return (0);
+}
+// int main()
+// {
+// 	std::cout << BMAG << "\n---- TEST 0 ----" << RESET << std::endl;
+// 	std::cout << BYEL << "case : Normal assign" << RESET << std::endl;
+// 	Form sheet0;
+// 	Form sheet1("A", 75, 50);
+// 	Form sheet2(sheet0);
+// 	Form sheet3 = sheet1;
+// 	std::cout << sheet0;
+// 	std::cout << sheet1;
+// 	std::cout << sheet2;
+// 	std::cout << sheet3;
+// }
