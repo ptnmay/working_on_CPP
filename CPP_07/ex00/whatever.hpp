3141fd4a1b6e57f90ddd27910b213cb76303d4dd
ptnmay <pittinansaeyang@gmail.com>
2024-01-05 06:49:53 +0700

    .hpp .cpp

---
+++ CPP_07/ex00/whatever.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psaeyang <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/05 06:48:25 by psaeyang          #+#    #+#             */
+/*   Updated: 2024/01/05 06:49:30 by psaeyang         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+# include <iostream>
+# include <typeinfo>
+# include <cstdlib>
+# include <time.h>
+# include <sstream>
+
+# define BBLK "\e[1;30m"
+# define BRED "\e[1;31m"
+# define BGRN "\e[1;32m"
+# define BYEL "\e[1;33m"
+# define BBLU "\e[1;34m"
+# define BMAG "\e[1;35m"
+# define BCYN "\e[1;36m"
+# define BWHT "\e[1;37m"
+# define RESET "\e[0m"
+
+
+
+#endif
