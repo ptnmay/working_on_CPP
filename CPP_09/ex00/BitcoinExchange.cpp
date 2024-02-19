7ce990427ba8afeec5a244b89c298840c26aabc8
ptnmay <pittinansaeyang@gmail.com>
2024-02-20 03:01:26 +0700

    -

--- CPP_09/ex00/BitcoinExchange.cpp
+++ CPP_09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: psaeyang <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 12:52:32 by psaeyang          #+#    #+#             */
-/*   Updated: 2024/02/20 00:27:42 by psaeyang         ###   ########.fr       */
+/*   Updated: 2024/02/20 01:21:10 by psaeyang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,29 @@ BitcoinExchange::BitcoinExchange(std::string const &av)
 
 	if (!create_data())
 		throw ErrorDatabase();
+}
+
+bool BitcoinExchange::create_data()
+{
+	std::string name = "data.csv";
+	std::ifstream file;
+	if (!check_data(name))
+		return (false);
+	file.open(name.c_str(), std::fstream::in);
+	if (!file.is_open())
+		return (false);
+	std::string line;
+	std::getline(file, line);
+	// while (std::getline(file, line))
+	// 	add_data(line);
+	file.close();
+	return (true);
+}
+
+bool check_data(std::string const& name)
+{
+	size_t n;
+	
 }
 
 BitcoinExchange::BitcoinExchange(BitcoinExchange const& other)
