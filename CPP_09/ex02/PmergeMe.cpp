7315f83ef41e89b5b5ea615affe1678742b077b0
Pittinan Saeyang <pittinansaeyang@gmail.com>
2024-03-28 04:55:43 +0700

    -

--- CPP_09/ex02/PmergeMe.cpp
+++ CPP_09/ex02/PmergeMe.cpp
@@ -6,7 +6,26 @@
 /*   By: psaeyang <psaeyang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 04:41:36 by psaeyang          #+#    #+#             */
-/*   Updated: 2024/03/28 04:41:39 by psaeyang         ###   ########.fr       */
+/*   Updated: 2024/03/28 04:55:33 by psaeyang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "PmergeMe.hpp"
+
+PmergeMe::PmergeMe() {}
+
+PmergeMe::~PmergeMe() {}
+
+PmergeMe::PmergeMe(PmergeMe const &other)
+{
+    *this = other;
+}
+
+PmergeMe& PmergeMe::operator=(PmergeMe const &cp)
+{
+    if (this != &cp)
+    {
+        
+    }
+    return (*this);
+}
--- CPP_09/ex02/PmergeMe.hpp
+++ CPP_09/ex02/PmergeMe.hpp
@@ -6,7 +6,49 @@
 /*   By: psaeyang <psaeyang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 04:41:47 by psaeyang          #+#    #+#             */
-/*   Updated: 2024/03/28 04:41:49 by psaeyang         ###   ########.fr       */
+/*   Updated: 2024/03/28 04:46:31 by psaeyang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PMERGEME_HPP
+# define PMERGEME_HPP
+
+# include <iostream>
+# include <stack>
+# include <sstream>
+# include <exception>
+# include <cstdlib>
+
+# define BBLK "\e[1;30m"
+# define BRED "\e[1;31m"
+# define BGRN "\e[1;32m"
+# define BYEL "\e[1;33m"
+# define BBLU "\e[1;34m"
+# define BMAG "\e[1;35m"
+# define BCYN "\e[1;36m"
+# define BWHT "\e[1;37m"
+# define RESET "\e[0m"
+
+
+class PmergeMe
+{
+	private:
+			std::stack<int> _stack;
+	public :
+			~PmergeMe();
+			PmergeMe();
+			PmergeMe(PmergeMe const& other);
+			PmergeMe& operator=(PmergeMe const &cp);
+
+	class Negnum : public std::exception
+	{
+		public:
+			virtual const char* what() const throw()
+			{
+				return (BRED"Error: Negative number");
+			}
+	};
+};
+
+
+#endif
